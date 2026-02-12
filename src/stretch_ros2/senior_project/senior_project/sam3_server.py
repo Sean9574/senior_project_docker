0be001e2051353d73dded589350043edad09f88f
Sean9574 <sean9574@gmail.com>
2026-02-11 20:41:13 -0500

    fixed path issue for sam3 server

--- src/stretch_ros2/senior_project/senior_project/sam3_server.py
+++ src/stretch_ros2/senior_project/senior_project/sam3_server.py
@@ -104,7 +104,7 @@ async def lifespan(app: FastAPI):
         os.environ["HUGGINGFACE_HUB_TOKEN"] = tok
         os.environ["HF_TOKEN"] = tok  # extra compatibility
 
-    model = build_sam3_image_model(checkpoint_path="/home/stretch/.cache/huggingface/hub/<PATH_TO_SAM3_CKPT_FILE>")
+    model = build_sam3_image_model()
     print("✓ Loaded model")
 
     
